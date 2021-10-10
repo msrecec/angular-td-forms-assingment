de8cc6394568eadc52d599b94fecbc1d455b3ab0
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-10 12:27:20 +0200

    finished assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  @ViewChild("f") signupForm: NgForm;
+  defaultSubscription = "advanced";
+  formData: {
+    email: string;
+    subscription: string;
+    password: string;
+  } = {
+    email: "",
+    subscription: "",
+    password: "",
+  };
 
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+
+    this.formData.email = this.signupForm.value.email;
+    this.formData.subscription = this.signupForm.value.subscription;
+    this.formData.password = this.signupForm.value.password;
+
+    this.signupForm.reset();
+  }
 }
